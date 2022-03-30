66ee2890201e0e0f83e5ecbeb543e35b5c313d22
Kommai <dev@kommai.app>
2022-03-30 17:00:59 +0900

    Modified

--- src/ValidatorTrait.php
+++ src/ValidatorTrait.php
@@ -4,12 +4,26 @@ declare(strict_types=1);
 
 namespace Kommai\Validation;
 
+use RuntimeException;
+
 trait ValidatorTrait
 {
     private array $rules = [];
 
-    public function validate(array $data)
+    public function validate(array $data): array
     {
-        // returns an array of errors
+        $errors = [];
+        foreach ($this->rules as $key => $rules) {
+            if (!isset($data[$key])) {
+                throw new RuntimeException(sprintf('The data is missing a value keyed "%s"', (string) $key));
+            }
+            foreach ($rules as $ruleset) {
+                if (call_user_func($ruleset['validator'], $data[$key]) !== true) {
+                    $errors[$key] = $ruleset['error'];
+                    break;
+                }
+            }
+        }
+        return $errors;
     }
 }
--- tests/input.php
+++ tests/input.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Kommai\Validation\InputValidationTrait;
 use Kommai\Validation\ValidatorTrait;
+use Kommai\TestKit\Proxy;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
@@ -18,15 +19,25 @@ $data = [
 ];
 
 $data = [
-    'name' => 'Alice',
+    'name' => '',
+    'email' => 'alice@example.com.123',
+    //'url' => 'example.com',
+    'password' => '12345678',
+    'postal' => '730-0854',
 ];
 
 $validator = new ExampleInputValidator();
 //$validator->filled(0, 'must be filled');
 //$validator->filled('X', 'must be filled');
-$validator
-->filled('name', 'must be filled')
-//->equal('name', 'Alice', 'must be Alice')
-;
+$validator->filled('name', 'must be filled')->equal('name', 'Alice', 'must be Alice');
+$validator->filled('email', 'must be filled')->email('email', 'invalid');
+if (isset($data['url'])) {
+    $validator->url('url', 'invalid');
+}
+$validator->filled('password', 'must be filled')->longer('password', 4 - 1, 'too short')->shorter('password', 8 + 1, 'too long');
+$validator->filled('postal', 'must be filled')->regex('postal', '/\A[0-9]{7}\z/', 'invalid');
+
+$validatorProxy = new Proxy($validator);
+//var_dump($validatorProxy->rules);
 
 var_dump($validator->validate($data));
