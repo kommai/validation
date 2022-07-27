f7a9a744e0eb06cd266e8c3930cc99d2953bf530
Kommai <dev@kommai.app>
2022-07-27 17:10:41 +0900

    Grabbed functions from traits

--- src/StringValidationTrait.php
+++ src/StringValidationTrait.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Kommai\Validation;
 
+use InvalidArgumentException;
+
 trait StringValidationTrait
 {
     private function filled(int|string $key, string $error): self
@@ -11,6 +13,17 @@ trait StringValidationTrait
         $this->rules[$key][] = [
             'validator' => function ($value) {
                 return strval($value) !== '';
+            },
+            'error' => $error,
+        ];
+        return $this;
+    }
+
+    private function equal(int|string $key, string $target, string $error): self
+    {
+        $this->rules[$key][] = [
+            'validator' => function ($value) use ($target) {
+                return $value === $target;
             },
             'error' => $error,
         ];
@@ -33,6 +46,43 @@ trait StringValidationTrait
         $this->rules[$key][] = [
             'validator' => function ($value) use ($length) {
                 return mb_strlen((string) $value) < $length;
+            },
+            'error' => $error,
+        ];
+        return $this;
+    }
+
+    private function email(int|string $key, string $error): self
+    {
+        $this->rules[$key][] = [
+            'validator' => function ($value) {
+                return filter_var($value, FILTER_VALIDATE_EMAIL) !== false;
+            },
+            'error' => $error,
+        ];
+        return $this;
+    }
+
+    private function url(int|string $key, string $error): self
+    {
+        $this->rules[$key][] = [
+            'validator' => function ($value) {
+                return filter_var($value, FILTER_VALIDATE_URL) !== false;
+            },
+            'error' => $error,
+        ];
+        return $this;
+    }
+
+    private function regex(int|string $key, string $pattern, string $error): self
+    {
+        $this->rules[$key][] = [
+            'validator' => function ($value) use ($pattern) {
+                $result = preg_match($pattern, $value);
+                if ($result === false) {
+                    throw new InvalidArgumentException('Invalid regular expression');
+                }
+                return $result === 1;
             },
             'error' => $error,
         ];
