3080367c1153cd9c11d973dcd9f985163d31134d
Kommai <dev@kommai.app>
2022-07-27 17:32:23 +0900

    Saved

--- src/UploadValidationTrait.php
+++ src/UploadValidationTrait.php
@@ -9,6 +9,31 @@ use RuntimeException;
 
 trait UploadValidationTrait
 {
+    // TODO: more validations
+    // uploaded -> fail when the file is not uploaded
+    // ?? -> fail when the file exceeds ini size (no need to use form size since form size can be easily modified)
+    private function succeeded(int|string $key, string $error): self
+    {
+        $this->rules[$key][] = [
+            'validator' => function (Upload $upload) {
+                return $upload->error !== UPLOAD_ERR_PARTIAL;
+            },
+            'error' => $error,
+        ];
+        return $this;
+    }
+
+    private function complete(int|string $key, string $error): self
+    {
+        $this->rules[$key][] = [
+            'validator' => function (Upload $upload) {
+                return $upload->error !== UPLOAD_ERR_PARTIAL;
+            },
+            'error' => $error,
+        ];
+        return $this;
+    }
+
     private function smaller(int|string $key, int $size, string $error): self
     {
         $this->rules[$key][] = [
